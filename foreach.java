dac3021cdf874f5a2e804b4e152b21222138d705
vietGS <giuseviet1411@gmail.com>
2022-03-21 11:09:29 +0700

    first commit

---
+++ foreach.java
@@ -0,0 +1,10 @@
+public class foreach{
+
+     public static void main(String []args){
+        int[] array = {1,2,4};
+
+        for (int a: array){
+            System.out.println(a);
+        }
+     }
+}
